4a7bbab17db88d5adbf625d9a13e497c7cdce1fd
Arno Rehn <arno@arnorehn.de>
2010-04-04 23:38:22 +0000

    initial hooks support
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/playground/bindings/qyotoassemblygen@1111159 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ QyotoHooks.cs
@@ -0,0 +1,35 @@
+using System;
+using System.CodeDom;
+
+unsafe class QyotoHooks {
+
+    static string qObjectDummyCtorCode =
+"            try {\n" +
+"                Type proxyInterface = Qyoto.GetSignalsInterface(GetType());\n" +
+"                SignalInvocation realProxy = new SignalInvocation(proxyInterface, this);\n" +
+"                Q_EMIT = realProxy.GetTransparentProxy();\n" +
+"            }\n" +
+"            catch (Exception e) {\n" +
+"                Console.WriteLine(\"Could not retrieve signal interface: {0}\", e);\n" +
+"            }";
+
+    public void PreMethodsHook(Smoke *smoke, Smoke.Class *klass, CodeTypeDeclaration type) {
+        if (type.Name == "QObject") {
+            // Add 'Qt' base class
+            type.BaseTypes.Add(new CodeTypeReference("Qt"));
+
+            // add the Q_EMIT field
+            CodeMemberField Q_EMIT = new CodeMemberField(typeof(object), "Q_EMIT");
+            Q_EMIT.Attributes = MemberAttributes.Family;
+            Q_EMIT.InitExpression = new CodePrimitiveExpression(null);
+            type.Members.Add(Q_EMIT);
+        }
+    }
+
+    public void SupportingMethodsHook(Smoke *smoke, Smoke.Method *method, CodeMemberMethod cmm, CodeTypeDeclaration type) {
+        if (type.Name == "QObject" && cmm is CodeConstructor) {
+            cmm.Statements.Add(new CodeSnippetStatement(qObjectDummyCtorCode));
+        }
+    }
+
+}
