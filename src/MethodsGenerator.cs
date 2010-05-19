c7137d1e43155b84d792fdb3d04d1e91b387d27b
Arno Rehn <arno@arnorehn.de>
2010-05-19 16:09:10 +0200

    update copyright & license information

--- src/MethodsGenerator.cs
+++ src/MethodsGenerator.cs
@@ -1,6 +1,6 @@
 /*
     Generator for .NET assemblies utilizing SMOKE libraries
-    Copyright (C) 2009 Arno Rehn <arno@arnorehn.de>
+    Copyright (C) 2009, 2010 Arno Rehn <arno@arnorehn.de>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
