c838007853f81ffe390be39e386da19ccfde7af8
Arno Rehn <arno@arnorehn.de>
2010-05-29 22:19:39 +0200

    add opengl

---
+++ assemblies/qyoto-qtopengl/native/qtopengl.cpp
@@ -0,0 +1,56 @@
+/***************************************************************************
+                          qtopengl.cpp  -  description
+                             -------------------
+    begin                : Wed Jun 16 2004
+    copyright            : (C) 2004 by Richard Dale
+    email                : Richard_Dale@tipitina.demon.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QByteArray>
+#include <QHash>
+
+#include <qyoto.h>
+#include <qyotosmokebinding.h>
+
+#include <qtopengl_smoke.h>
+
+static bool IsContainedInstanceQtOpenGL(smokeqyoto_object *o)
+{
+    return false;
+}
+
+static const char * qyoto_resolve_classname_qtopengl(smokeqyoto_object * o)
+{
+    return qyoto_modules[o->smoke].binding->className(o->classId);
+}
+
+
+extern "C" Q_DECL_EXPORT void
+Init_qyoto_qtopengl()
+{
+    init_qtopengl_Smoke();
+
+    QByteArray prefix("Qyoto.");
+
+    QHash<int,char *> qtopengl_classname;
+    for (int i = 1; i <= qtopengl_Smoke->numClasses; i++) {
+        QByteArray name(qtopengl_Smoke->classes[i].className);
+        name.replace("::", ".");
+        if (name != "QGL") {
+            name.prepend(prefix);
+        }
+        qtopengl_classname.insert(i, strdup(name.constData()));
+    }
+    static Qyoto::Binding binding = Qyoto::Binding(qtopengl_Smoke, qtopengl_classname);
+    QyotoModule module = { "qyoto_qtopengl", qyoto_resolve_classname_qtopengl, IsContainedInstanceQtOpenGL, &binding };
+    qyoto_modules[qtopengl_Smoke] = module;
+}
---
+++ assemblies/qyoto-qtopengl/qyoto-qtopengl.pc.cmake
@@ -0,0 +1,4 @@
+Name: Qyoto QtOpenGL
+Description: QtOpenGL .NET/Mono bindings
+Version: 2.0.0
+Libs: -r:@assembly@
