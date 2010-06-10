5d19fe0dfcae757cec7d169d98bfd0250552aed4
Arno Rehn <arno@arnorehn.de>
2010-06-11 01:14:41 +0200

    this hackish code is not needed anymore with recent SMOKE versions

--- assemblies/kimono-kdecore/KPluginFactoryExtras.cs
+++ assemblies/kimono-kdecore/KPluginFactoryExtras.cs
@@ -1,12 +1,9 @@
 namespace Kimono {
 	using System;
 	using System.Collections.Generic;
-	using System.Runtime.InteropServices;
 	using Qyoto;
 
 	public partial class KPluginFactory : QObject, IDisposable {
-		[DllImport("kimono-kdecore-native", CharSet=CharSet.Ansi)]
-		static extern IntPtr KPluginFactory_Create(IntPtr self, string classname, IntPtr parentWidget, IntPtr parent, IntPtr[] args, int numArgs, string keyword);
 
 		public T Create<T>() where T: class {
 			return Create<T>((QObject) null);
@@ -48,30 +45,20 @@ namespace Kimono {
 
 		public T Create<T>(QWidget parentWidget, QObject parent, string keyword, List<QVariant> args) where T: class {
 			Type type = typeof(T);
-			
+
 			if (!SmokeMarshallers.IsSmokeClass(type))
 				throw new Exception("The generic type must be included in the bindings");
-			
-			IntPtr[] array = new IntPtr[args.Count];
-			for (int i = 0; i < args.Count; i++) {
-				array[i] = (IntPtr) GCHandle.Alloc(args[i]);
+
+			QObject o = Create(SmokeMarshallers.SmokeClassName(type), parentWidget, parent, args, keyword);
+			if (o == null) {
+				return null;
 			}
-			
-			IntPtr pw = IntPtr.Zero;
-			if (parentWidget != null)
-				pw = (IntPtr) GCHandle.Alloc(parentWidget);
-
-			IntPtr p = IntPtr.Zero;
-			if (parent != null)
-				p = (IntPtr) GCHandle.Alloc(parent);
-
-			IntPtr ret = KPluginFactory_Create((IntPtr) GCHandle.Alloc(this), SmokeMarshallers.SmokeClassName(type), pw, p,
-			                                   array, array.Length, keyword);
-			QObject o = (ret != IntPtr.Zero) ? (QObject) ((GCHandle) ret).Target : null;
 			T t = qobject_cast<T>(o);
-			if (t == null && o != null)
+			if (t == null)
 				o.Dispose();
 			return t;
 		}
 	}
 }
+
+// kate: space-indent off;
