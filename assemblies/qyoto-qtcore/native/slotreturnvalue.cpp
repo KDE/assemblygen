395c9da3400f96686a1976b4c8a460354a5d0623
Dimitar Dobrev <dpldobrev@yahoo.com>
2012-05-08 21:57:44 +0300

    Reverted some wrong changes.
    Signed-off-by: ddobrev <dpldobrev@yahoo.com>

--- assemblies/qyoto-qtcore/native/slotreturnvalue.cpp
+++ assemblies/qyoto-qtcore/native/slotreturnvalue.cpp
@@ -13,7 +13,7 @@
  *                                                                         *
  ***************************************************************************/
 
-//#include <QtDBus>
+#include <QtDBus>
 
 #include "slotreturnvalue.h"
 #include "qyoto.h"
@@ -22,15 +22,18 @@ namespace Qyoto {
 
 SlotReturnValue::SlotReturnValue(void ** o, Smoke::StackItem * result, QList<MocArgument*> replyType)
 {
-	_result = result;
-	_replyType = replyType;
-	_stack = new Smoke::StackItem[1]; 
-	Marshall::HandlerFn fn = getMarshallFn(type());
-	(*fn)(this);
-	
-	QByteArray t(type().name());
-	t.replace("const ", "");
-        t.replace("&", "");
+    _result = result;
+    _replyType = replyType;
+    _stack = new Smoke::StackItem[1];
+    Marshall::HandlerFn fn = getMarshallFn(type());
+    (*fn)(this);
+
+    QByteArray t(type().name());
+    t.replace("const ", "");
+    t.replace("&", "");
+    if (t == "QDBusVariant") {
+        *reinterpret_cast<QDBusVariant*>(o[0]) = *(QDBusVariant*) _stack[0].s_class;
+    } else {
         // Save any address in zeroth element of the arrary of 'void*'s passed to
         // qt_metacall()
         void * ptr = o[0];
@@ -38,21 +41,22 @@ SlotReturnValue::SlotReturnValue(void ** o, Smoke::StackItem * result, QList<Moc
         // Only if the zeroth element of the array of 'void*'s passed to qt_metacall()
         // contains an address, is the return value of the slot needed.
         if (ptr != 0) {
-                *(void**)ptr = *(void**)(o[0]);
+            *(void**)ptr = *(void**)(o[0]);
         }
+    }
 }
 
 void
-SlotReturnValue::unsupported() 
+SlotReturnValue::unsupported()
 {
-	qFatal("Cannot handle '%s' as slot reply-type", type().name());
+    qFatal("Cannot handle '%s' as slot reply-type", type().name());
 }
 
 void
 SlotReturnValue::next() {}
 
 SlotReturnValue::~SlotReturnValue() {
-	delete[] _stack;
+    delete[] _stack;
 }
 
 }
