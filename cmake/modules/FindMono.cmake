61956049264b9c608b9dbfdc9006f287b97737cb
Dimitar Dobrev <dpldobrev@yahoo.com>
2012-05-13 20:02:40 +0300

    From Steven Boswell: makes the build look for the .NET 4 compiler under both Windows and Linux.
    Signed-off-by: ddobrev <dpldobrev@yahoo.com>

--- cmake/modules/FindMono.cmake
+++ cmake/modules/FindMono.cmake
@@ -1,10 +1,10 @@
-# - Try to find the mono, mcs, gmcs and gacutil
+# - Try to find the mono, mcs, dmcs and gacutil
 #
 # defines
 #
-# MONO_FOUND - system has mono, mcs, gmcs and gacutil
+# MONO_FOUND - system has mono, mcs, dmcs and gacutil
 # MONO_PATH - where to find 'mono'
-# GMCS_PATH - where to find 'gmcs'
+# GMCS_PATH - where to find 'dmcs'
 # GACUTIL_PATH - where to find 'gacutil'
 #
 # copyright (c) 2007 Arno Rehn arno@arnorehn.de
@@ -12,25 +12,25 @@
 # Redistribution and use is allowed according to the terms of the GPL license.
 
 FIND_PROGRAM (MONO_EXECUTABLE mono)
-FIND_PROGRAM (GMCS_EXECUTABLE gmcs)
+FIND_PROGRAM (DMCS_EXECUTABLE dmcs)
 FIND_PROGRAM (GACUTIL_EXECUTABLE gacutil)
 
 SET (MONO_FOUND FALSE CACHE INTERNAL "")
 
-IF (MONO_EXECUTABLE AND GMCS_EXECUTABLE AND GACUTIL_EXECUTABLE)
+IF (MONO_EXECUTABLE AND DMCS_EXECUTABLE AND GACUTIL_EXECUTABLE)
 	SET (MONO_FOUND TRUE CACHE INTERNAL "")
-ENDIF (MONO_EXECUTABLE AND GMCS_EXECUTABLE AND GACUTIL_EXECUTABLE)
+ENDIF (MONO_EXECUTABLE AND DMCS_EXECUTABLE AND GACUTIL_EXECUTABLE)
 
 IF (NOT Mono_FIND_QUIETLY)
     MESSAGE(STATUS "Path of mono: ${MONO_EXECUTABLE}")
-    MESSAGE(STATUS "Path of gmcs: ${GMCS_EXECUTABLE}")
+    MESSAGE(STATUS "Path of dmcs: ${DMCS_EXECUTABLE}")
     MESSAGE(STATUS "Path of gacutil: ${GACUTIL_EXECUTABLE}")
 ENDIF (NOT Mono_FIND_QUIETLY)
 
 IF (NOT MONO_FOUND)
 	IF (Mono_FIND_REQUIRED)
-		MESSAGE(FATAL_ERROR "Could not find one or more of the following programs: mono, gmcs, gacutil")
+		MESSAGE(FATAL_ERROR "Could not find one or more of the following programs: mono, dmcs, gacutil")
 	ENDIF (Mono_FIND_REQUIRED)
 ENDIF (NOT MONO_FOUND)
 
-MARK_AS_ADVANCED(MONO_EXECUTABLE GMCS_EXECUTABLE GACUTIL_EXECUTABLE)
+MARK_AS_ADVANCED(MONO_EXECUTABLE DMCS_EXECUTABLE GACUTIL_EXECUTABLE)
