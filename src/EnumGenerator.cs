ac2a75c9d03ff01875fe89e705f8d5b58ab9d09d
Dimitar Dobrev <dpldobrev@yahoo.com>
2012-10-17 13:19:02 +0300

    Refactored the enum generator.
    Signed-off-by: ddobrev <dpldobrev@yahoo.com>

--- src/EnumGenerator.cs
+++ src/EnumGenerator.cs
@@ -19,101 +19,117 @@
 
 using System;
 using System.Runtime.InteropServices;
-using System.Collections;
-using System.Collections.Generic;
 using System.CodeDom;
 
 // Generates C# enums from enums found in the smoke lib.
-public unsafe class EnumGenerator {
+public unsafe class EnumGenerator
+{
 
-    [DllImport("assemblygen-native", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)]
-    static extern unsafe long GetEnumValue(Smoke* smoke, Smoke.Method* meth);
+	[DllImport("assemblygen-native", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+	private static extern long GetEnumValue(Smoke* smoke, Smoke.Method* meth);
 
-    readonly GeneratorData data;
+	private readonly GeneratorData data;
 
-    public EnumGenerator(GeneratorData data) {
-        this.data = data;
-    }
+	public EnumGenerator(GeneratorData data)
+	{
+		this.data = data;
+	}
 
-    /*
-     * Defines an Enum.
-     */
-    CodeTypeDeclaration DefineEnum(string cppName) {
-        int colon = cppName.LastIndexOf("::");
-        string prefix = string.Empty;
-        if (colon != -1) {
-            prefix = cppName.Substring(0,  colon);
-        }
+	/*
+	 * Defines an Enum.
+	 */
 
-        string name = cppName;
-        if (colon != -1) {
-            name = cppName.Substring(colon + 2);
-        }
+	private CodeTypeDeclaration DefineEnum(string cppName)
+	{
+		int colon = cppName.LastIndexOf("::", StringComparison.Ordinal);
+		string prefix = string.Empty;
+		if (colon != -1)
+		{
+			prefix = cppName.Substring(0, colon);
+		}
 
-        CodeTypeDeclaration typeDecl = new CodeTypeDeclaration(name);
-        typeDecl.IsEnum = true;
-        if (!data.ReferencedTypeMap.ContainsKey(name) || 
-            data.ReferencedTypeMap[name].FullName != data.DefaultNamespace.Name + "." + cppName.Replace("::", "+")) {
-            data.GetTypeCollection(prefix).Add(typeDecl);
-            data.EnumTypeMap[cppName] = typeDecl;
-        }
-        return typeDecl;
-    }
+		string name = cppName;
+		if (colon != -1)
+		{
+			name = cppName.Substring(colon + 2);
+		}
 
-    /*
-     * convenience overload
-     */
-    CodeTypeDeclaration DefineEnum(Smoke.Type* type) {
-        // we want the exact combination: t_enum | tf_stack
-        if (type->flags != ((uint) Smoke.TypeId.t_enum | (uint) Smoke.TypeFlags.tf_stack)) {
-            // not an enum type
-            return null;
-        }
+		CodeTypeDeclaration typeDecl = new CodeTypeDeclaration(name);
+		typeDecl.IsEnum = true;
+		if (!data.ReferencedTypeMap.ContainsKey(name) ||
+		    data.ReferencedTypeMap[name].FullName != data.DefaultNamespace.Name + "." + cppName.Replace("::", "+"))
+		{
+			data.GetTypeCollection(prefix).Add(typeDecl);
+			data.EnumTypeMap[cppName] = typeDecl;
+		}
+		return typeDecl;
+	}
 
-        if (type->classId == 0 || data.Smoke->classes[type->classId].external) {
-            // defined elsewhere
-            return null;
-        }
+	/*
+	 * convenience overload
+	 */
 
-        string enumName = ByteArrayManager.GetString(type->name);
+	private void DefineEnum(Smoke.Type* type)
+	{
+		// we want the exact combination: t_enum | tf_stack
+		if (type->flags != ((uint) Smoke.TypeId.t_enum | (uint) Smoke.TypeFlags.tf_stack))
+		{
+			// not an enum type
+			return;
+		}
 
-        return DefineEnum(enumName);
-    }
+		if (type->classId == 0 || data.Smoke->classes[type->classId].external)
+		{
+			// defined elsewhere
+			return;
+		}
 
-    /*
-     * Loops through the 'types' table and defines .NET Enums for t_enums
-     */
-    public void DefineEnums() {
-        for (short i = 1; i <= data.Smoke->numTypes; i++) {
-            DefineEnum(data.Smoke->types + i);
-        }
-    }
+		string enumName = ByteArrayManager.GetString(type->name);
 
-    /*
-     * Generates an Enum member, creating the Enum if necessary.
-     */
-    public void DefineMember(Smoke.Method* meth) {
-        if ((meth->flags & (uint) Smoke.MethodFlags.mf_enum) == 0)
-            return;
+		this.DefineEnum(enumName);
+	}
 
-        string typeName = ByteArrayManager.GetString(data.Smoke->types[meth->ret].name);
-        if (typeName == "long") // unnamed enum
-            return;
+	/*
+	 * Loops through the 'types' table and defines .NET Enums for t_enums
+	 */
 
-        CodeTypeDeclaration enumType;
-        if (!data.EnumTypeMap.TryGetValue(typeName, out enumType)) {
-            enumType = DefineEnum(typeName);
-        }
-        CodeMemberField member = new CodeMemberField();
-        member.Name = ByteArrayManager.GetString(data.Smoke->methodNames[meth->name]);
-        long value = GetEnumValue(data.Smoke, meth);
+	public void DefineEnums()
+	{
+		for (short i = 1; i <= data.Smoke->numTypes; i++)
+		{
+			DefineEnum(data.Smoke->types + i);
+		}
+	}
 
-        if (value > int.MaxValue && enumType.BaseTypes.Count == 0) {
-            // make the enum derive from 'long' if necessary
-            enumType.BaseTypes.Add(new CodeTypeReference(typeof(long)));
-        }
+	/*
+	 * Generates an Enum member, creating the Enum if necessary.
+	 */
 
-        member.InitExpression = new CodePrimitiveExpression(value);
-        enumType.Members.Add(member);
-    }
+	public void DefineMember(Smoke.Method* meth)
+	{
+		if ((meth->flags & (uint) Smoke.MethodFlags.mf_enum) == 0)
+			return;
+
+		string typeName = ByteArrayManager.GetString(data.Smoke->types[meth->ret].name);
+		if (typeName == "long") // unnamed enum
+			return;
+
+		CodeTypeDeclaration enumType;
+		if (!data.EnumTypeMap.TryGetValue(typeName, out enumType))
+		{
+			enumType = DefineEnum(typeName);
+		}
+		CodeMemberField member = new CodeMemberField();
+		member.Name = ByteArrayManager.GetString(data.Smoke->methodNames[meth->name]);
+		long value = GetEnumValue(data.Smoke, meth);
+
+		if (value > int.MaxValue && enumType.BaseTypes.Count == 0)
+		{
+			// make the enum derive from 'long' if necessary
+			enumType.BaseTypes.Add(new CodeTypeReference(typeof(long)));
+		}
+
+		member.InitExpression = new CodePrimitiveExpression(value);
+		enumType.Members.Add(member);
+	}
 }
