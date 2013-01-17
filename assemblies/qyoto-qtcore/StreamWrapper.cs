6eaa712c6719cf7b6f1ee59680af1a1bae6ac2b6
Dimitar Dobrev <dpldobrev@yahoo.com>
2013-01-17 11:38:03 +0200

    Small improvements to the StreamWrapper. Removed the dash-containing words from the verbs and added "jump" as one.
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

--- assemblies/qyoto-qtcore/StreamWrapper.cs
+++ assemblies/qyoto-qtcore/StreamWrapper.cs
@@ -1,11 +1,15 @@
-namespace QtCore {
+namespace QtCore
+{
 	using System;
 	using System.IO;
-	
-	public class StreamWrapper : QIODevice {
+
+	public class StreamWrapper : QIODevice
+	{
 		private Stream m_stream;
 
-		public StreamWrapper(Stream stream, bool open) : base() {
+		public StreamWrapper(Stream stream, bool open = true)
+			: base()
+		{
 			m_stream = stream;
 			if (!open) return;
 			if (stream.CanRead && stream.CanWrite)
@@ -16,46 +20,48 @@ namespace QtCore {
 				Open(OpenModeFlag.WriteOnly);
 		}
 
-		public StreamWrapper(Stream stream) : this(stream, true) {}
-
-		protected override unsafe long ReadData(Pointer<sbyte> data, long maxsize) {
-			int max = (maxsize > int.MaxValue)? int.MaxValue : (int) maxsize;
+		protected override unsafe long ReadData(Pointer<sbyte> data, long maxsize)
+		{
+			int max = (maxsize > int.MaxValue) ? int.MaxValue : (int) maxsize;
 			byte[] buffer = new byte[max];
 			int read = m_stream.Read(buffer, 0, max);
-			for (int i = 0; i < max; i++) {
+			for (int i = 0; i < max; i++)
+			{
 				data[i] = (sbyte) buffer[i];
 			}
 			return read;
 		}
 
-		protected override long WriteData(string data, long maxsize) {
-			int max = (maxsize > int.MaxValue)? int.MaxValue : (int) maxsize;
-			int i = 0;
-			for (; i < max; i++)
+		protected override long WriteData(string data, long maxsize)
+		{
+			int max = (maxsize > int.MaxValue) ? int.MaxValue : (int) maxsize;
+			for (int i = 0; i < max; i++)
+			{
 				m_stream.WriteByte((byte) data[i]);
-			return i;
-		}
-		
-		public override bool Seek(long pos) {
-			base.Seek(pos);
-			try {
-				m_stream.Seek(pos, SeekOrigin.Begin);
-				return true;
-			} catch {
-				return false;
 			}
+			return max;
 		}
-		
-		public override long Size {
+
+		public override bool Seek(long pos)
+		{
+			base.Seek(pos);
+			m_stream.Seek(pos, SeekOrigin.Begin);
+			return true;
+		}
+
+		public override long Size
+		{
 			get
 			{
 				if (!m_stream.CanSeek) return base.Size;
 				return m_stream.Length;
 			}
 		}
-		
-		public override void Close() {
+
+		public override void Close()
+		{
 			m_stream.Close();
+			base.Close();
 		}
 	}
 }
